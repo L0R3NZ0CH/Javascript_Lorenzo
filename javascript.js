66757e74a39799183d4515828a7d017136aeb88a
L0R3NZ0CH <rdzcha77@gmail.com>
2023-10-04 13:32:00 -0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,18 @@
+// var numero = 5;
+// var numero = 4;
+// let numero = 5;
+// numero = 8;
+// const constante = 4;
+// constante = 10;
+let nombre = 'Lorenzo';
+let apellido = ' Chavez';
+console.log('mi nombre es: '+  nombre+ apellido);
+// document.write ('mi nombre es: '+  nombre+ apellido);
+let seleccion = document.querySelector('#seccion1');
+seleccion.innerHTML= 'Mi nombre es :' +nombre + apellido;
+
+       
+       
+    //    alert('Hola desde una alerta de JavaScript');
+        // console.log('Esto es JavaScript desde el HTML');
+        // console.log(numero);
